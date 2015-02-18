af17e90fa63149de0393b7b890ce35688583ed47
Fakhertdinov Marat <fakhertdinov.marat@gmail.com>
2015-02-18 09:25:17 +0600

    Added method of page 404 to TemplateController.

--- src/TemplateController.php
+++ src/TemplateController.php
@@ -3,7 +3,6 @@
 namespace Butterfly\Plugin\TemplateRouter;
 
 use Butterfly\Adapter\Twig\IRenderer;
-use Butterfly\Component\DI\Container;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,38 +14,61 @@ class TemplateController
     /**
      * @var IRenderer
      */
-    protected $render;
+    protected $renderer;
 
     /**
-     * @var Container
+     * @var array
      */
-    protected $container;
+    protected $data;
 
     /**
-     * @param IRenderer $render
-     * @param Container $container
+     * @var string|null
      */
-    public function __construct(IRenderer $render, Container $container)
+    protected $templateOf404;
+
+    /**
+     * @param IRenderer $renderer
+     * @param array $data
+     * @param string $templateOf404
+     */
+    public function __construct(IRenderer $renderer, array $data, $templateOf404 = null)
     {
-        $this->container = $container;
-        $this->render    = $render;
+        $this->renderer      = $renderer;
+        $this->data          = $data;
+        $this->templateOf404 = $templateOf404;
     }
 
     /**
      * @param Request $request
-     * @return string
+     * @return Response
      */
     public function indexAction(Request $request)
     {
         $template = $request->attributes->get('template');
 
-        $parameters = array(
-            'container' => $this->container,
-            'data'      => $this->container->getParameter('bfy_plugin.template_router.data_source'),
-            'request'   => $request,
-        );
+        return $this->render($template, $this->data);
+    }
 
-        $content = $this->render->render($template, $parameters);
+    /**
+     * @return Response
+     */
+    public function page404Action()
+    {
+        if (empty($this->templateOf404)) {
+            return new Response('404 - Page not found');
+        }
+
+        return $this->render($this->templateOf404, $this->data);
+    }
+
+    /**
+     * @param string $template
+     * @param array $parameters
+     * @return Response
+     */
+    protected function render($template, array $parameters)
+    {
+        $content = $this->renderer->render($template, $parameters);
 
         return new Response($content);
     }
